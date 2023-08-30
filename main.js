31fc1dbfa2dcc8d3f69397dad8e6d68b4356f62a
Amadeo <ramirez.amadeo1@gmail.com>
2023-08-29 19:49:03 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const result1 = 1 * 10;
+const result2 = 10 + 30;
+const result3 = 40 - 1;
+const world = "World!";
+
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+console.log(`Hello, ${world}! ${result1}-${result2}-${result3}`)
